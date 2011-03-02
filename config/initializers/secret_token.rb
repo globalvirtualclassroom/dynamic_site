3d553332f33d12bc23c02c480eefdf8033c90cc9
Felix Campbell <felixcampbell354@gmail.com>
2011-03-01 21:19:57 -0600

    Syncronized with Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gvc::Application.config.secret_token = 'a07e67f48b8eaa9cbed40662d5833f179c9b3de9defc495c8bcd0da3711d835655c1118132a7e75410ede9b4a0fe5e5c60e9f274012aff5da504e51d43e3f60d'
